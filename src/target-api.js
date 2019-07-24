e38258d9230b32a0af07ce5193afc8b5305ce8f5
mdrum <mdrum@psi.edu>
2019-07-24 14:40:57 -0700

    add initial target lookup API

--- src/target-api.js
+++ src/target-api.js
@@ -1,7 +1,32 @@
-import web from 'axios';
-import desolrize from 'desolrize.js'
+import router from 'router.js'
+import {httpGetAll, httpGet} from 'common-api.js'
 
-const solrUrl = 'https://sbnpds4.psi.edu/solr/'
-const webUICollection = 'sbn'
-const coreCollection = 'pds'
-const query = 'select'
+export function lookupTarget(lidvid) {
+    let [lid, vid] = lidvid ? lidvid.split('::') : [null,null]
+    if(!lid) {
+        return new Promise((_, reject) => reject(new Error("Expected target parameter")))
+    }
+    let escapedLid = lid.replace(/:/g, '\\:')
+
+    return httpGetAll([
+        {
+            url: router.targetsCore,
+            params: {
+                q: `identifier:"${escapedLid}"`,
+            }
+        },
+        {
+            url: router.targetsWeb,
+            params: {
+                q: `logical_identifier:"${escapedLid}${vid ? '\\:\\:' + vid : ''}"`
+            }
+        }
+    ])
+}
+
+export function missionsForTarget(lid) {
+    let params = {
+        q: `(target_ref:"${lid}" +data_class:"Investigation")`
+    }
+    return httpGet(router.targetsCore, params)
+}
