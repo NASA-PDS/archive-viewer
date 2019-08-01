acd2c0411a24b9a1a7e0791e9c1781f132022b99
Conor Kingston <cmkingston21@me.com>
2019-08-01 11:36:22 -0700

    instrument page wired together

--- src/components/Instrument.js
+++ src/components/Instrument.js
@@ -2,68 +2,38 @@ import React from 'react';
 import {getSpacecraftForInstrument, getDatasetsForInstrument, getRelatedInstrumentsForInstrument} from 'api/instrument.js'
 import {getInstrumentsForSpacecraft} from 'api/spacecraft'
 import {Header, Description} from 'components/ContextObjects'
-import ListBox from 'components/ListBox'
+import ShowListBox from 'components/ListBox'
 
 export default class Instrument extends React.Component {
     constructor(props) {
         super(props)
-        const instrument = props.instrument
         this.state = {
-            instrument: instrument,
+            instrument: props.instrument,
+            datasets: null,
+            spacecraft: null,
+            instruments: null,
             loaded: false,
         }
     }
 
     componentDidMount() {
-        getSpacecraftForInstrument(this.state.instrument).then(spacecraft => {
-            console.log(spacecraft)
-            getRelatedInstrumentsForInstrument(this.state.instrument, spacecraft).then(console.log)
-        })
-        getDatasetsForInstrument(this.state.instrument).then(console.log)
+        getSpacecraftForInstrument(this.state.instrument).then(spacecraft => this.setState({spacecraft}))
+        getDatasetsForInstrument(this.state.instrument).then(datasets => this.setState({datasets}))
+        getRelatedInstrumentsForInstrument(this.state.instrument).then(instruments => this.setState({instruments}))
     }
 
     render() {
-        const {instrument} = this.state
+        const {instrument,datasets,spacecraft,instruments} = this.state
         return (
             <div>
                 <Header model={instrument} />
                 <Description model={instrument} />
-
+                <main className="co-main instrument-main">
+                    { ShowListBox( datasets, 'datasets' ) }
+                    { ShowListBox( spacecraft, 'spacecraft' ) }
+                    { ShowListBox( instruments, 'instruments') }
+                </main>
             </div>
         )
     }
 }
-
-class InstrumentList extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            instruments: props.instruments,
-            elements: [],
-            loaded: false
-        }
-    }
-    
-    render() {
-        let self = this
-        const {instruments} = self.state
-        self.state.elements = [];
-        let arr = Array.from(instruments)
-        
-        for (const [idx,val] of arr.entries()) {
-            const lid = val.identifier
-            const link = `/?instrument=${lid}`
-            
-            self.state.elements.push(<li key={val.title}><a href={link}>{val.title}</a></li>)
-        }
-        
-        return (
-            <section className="co-section instrument-list">
-                <h2>Instruments</h2>
-                <ListBox itemList={self.state.elements} />
-            </section>
-        )
-    }
-}
-
-export {Instrument, InstrumentList}
