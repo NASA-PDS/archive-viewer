4e7b692149b5dbe3f21d75cf5fdff82d521dfa24
Conor Kingston <cmkingston21@me.com>
2019-08-01 11:03:29 -0700

    filled in mission page

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import {getSpacecraftForMission, getTargetsForMission} from 'api/mission.js'
 import {Header, Description} from 'components/ContextObjects'
+import ShowListBox from 'components/ListBox'
 
 export default class Mission extends React.Component {
     constructor(props) {
         super(props)
-        const mission = props.mission
         this.state = {
-            mission: mission,
+            mission: props.mission,
+            datasets: null,
+            spacecraft: null,
+            instruments: null,
+            targets: null,
             loaded: false,
         }
     }
 
     componentDidMount() {
-        getSpacecraftForMission(this.state.mission).then(spacecraft => {
-            console.log(spacecraft)
-        })
-        getTargetsForMission(this.state.mission).then(targets => {
-            console.log(targets)
-        })
+        getSpacecraftForMission(this.state.mission).then(spacecraft => this.setState({spacecraft}))
+        getTargetsForMission(this.state.mission).then(targets => this.setState({targets}))
     }
 
     render() {
-        const {mission} = this.state
+        const {mission,datasets,spacecraft,instruments,targets} = this.state
+        
         return (
             <div>
                 <Header model={mission} />
                 <Description model={mission} />
-
+                <main className="co-main mission-main">
+                    { ShowListBox( spacecraft, 'spacecraft') }
+                    { ShowListBox( targets, 'targets') }
+                </main>
             </div>
         )
     }
