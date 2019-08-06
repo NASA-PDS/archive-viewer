b1f3e484fea6426d05f3102f785f6cafe4d19678
mdrum <mdrum@psi.edu>
2019-08-06 15:08:14 -0700

    adjust rules for showing mission/spacecraft info

--- src/components/Spacecraft.js
+++ src/components/Spacecraft.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'css/ContextObjects.scss'
 import {getMissionsForSpacecraft, getTargetsForSpacecraft, getInstrumentsForSpacecraft, getDatasetsForSpacecraft} from 'api/spacecraft.js'
-import {getSpacecraftForTarget} from 'api/target'
 import ListBox from 'components/ListBox'
 import {Header, Description} from 'components/ContextObjects'
 import Loading from 'components/Loading'
@@ -23,24 +22,28 @@ export default class Spacecraft extends React.Component {
     componentDidMount() {
         getTargetsForSpacecraft(this.state.spacecraft).then(targets => this.setState({targets}))
         getInstrumentsForSpacecraft(this.state.spacecraft).then(instruments => this.setState({instruments}))
-        getMissionsForSpacecraft(this.state.spacecraft).then(missions => this.setState({missions: missions,mission: missions && missions.length > 0 ? missions[0] : null}))
+        getMissionsForSpacecraft(this.state.spacecraft).then(missions => this.setState({mission: (missions && missions.length > 0) ? missions[0] : null}))
         getDatasetsForSpacecraft(this.state.spacecraft).then(datasets => this.setState({datasets}))
     }
 
     render() {
         const {spacecraft,mission,missions,datasets,instruments,targets} = this.state
-        if (!spacecraft || datasets === null || targets === null || instruments === null || mission === null || missions === null) return <Loading />
+        if (!spacecraft || datasets === null || targets === null || instruments === null || mission === null) return <Loading />
         else {
             return (
                 <div>
                     <Header model={mission} type={Header.type.mission} />
                     <main className="co-main target-main">
                         <div><Description model={mission} type={Description.type.mission} /></div>
-                        <Header model={spacecraft} type={Header.type.spacecraft}/>
+                        {mission.instrument_host_ref && mission.instrument_host_ref.length > 1 &&
+                            <Header model={spacecraft} type={Header.type.spacecraft}/>
+                        }
                         <ListBox type="dataset" items={datasets} groupBy="instrument" groupInfo={instruments} />
                     </main>
                     <aside className="sidebox">
-                        <ListBox type="mission"    items={missions} />
+                        {mission && 
+                            <h3><a href={`?mission=${mission.identifier}`}>Visit Mission Page</a></h3>
+                        }
                         <ListBox type="target"     items={targets} />
                         <ListBox type="instrument" items={instruments} />
                     </aside>
