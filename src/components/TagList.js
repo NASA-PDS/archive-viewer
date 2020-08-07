15680cb9f519732d499ae09380e7dd3db47d1907
mdrum <mdrum@psi.edu>
2020-08-07 16:54:40 -0700

    use material UI in tag list

--- src/components/TagList.js
+++ src/components/TagList.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import {TagTypes} from 'components/TagSearch.js'
+import { Chip, Link, Typography, Box, Hidden } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+    header: {
+      display: 'inline-block',
+      height: '100%',
+      verticalAlign: "middle"
+    },
+    chip: {
+      margin: theme.spacing(0.5),
+    }
+}));
+  
 function TagList({tags, type}) {
+    const classes = useStyles();
     if(!tags || tags.length === 0) return null
 
     return (
-        <div id="taxonomy">
-            <h3>Relevant Tags: </h3>
+        <Box>
+            <Hidden mdDown><Typography component="span" variant="h5" className={classes.header}> Relevant Tags: </Typography></Hidden>
             {tags.map(tag => 
-                <div className="banner">
-                    <a className="ignore-a-styling" href={`?tag=${tag}&type=${type}`}>
-                        <span>{tag}</span>
-                    </a>
-                </div>
+                <Link href={`?tag=${tag}&type=${type}`}>
+                    <Chip className={classes.chip} clickable="true" color="primary" label={tag}/>
+                </Link>
             )}
-        </div>
+        </Box>
     )
 }
 
