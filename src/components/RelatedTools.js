bdbd1919412ce75695ed74442331a82a08863b6e
mdrum <mdrum@psi.edu>
2020-08-07 11:56:00 -0700

    update related tools with material UI

--- src/components/RelatedTools.js
+++ src/components/RelatedTools.js
@@ -1,27 +1,39 @@
 import React from 'react';
+import { Link, Grid, CardMedia, Typography, Card, CardContent } from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    tool: {
+        maxWidth: 150,
+    },
+});
 
 export default function RelatedTools({tools}){
     if(!tools) { return null }
     return (
-        <div>
-        <h2>Useful tool{tools.length > 1 ? 's' : ''} for this data:</h2>
-        <ul className="related-tools" >
-            {tools.map(tool => (
-                <li key={tool.toolId}><ToolLink tool={tool}/></li>
-            ))}
-        </ul>
-        </div>
+        <>
+            <Typography gutterBottom variant="h4" component="h2">Useful tool{tools.length > 1 ? 's' : ''} for this data:</Typography>
+            <Grid container spacing={2} direction="row" justify="flex-start" alignItems="stretch">
+                {tools.map(tool => (
+                    <Grid item xs={6} md={2} ><ToolLink key={tool.toolId} tool={tool}/></Grid>
+                ))}
+            </Grid>
+        </>
     )
 }
 
 function ToolLink({tool}) {
+    const classes = useStyles();
     return (
-        <a className="related-tool" href={tool.directUrl ? tool.directUrl : tool.url}>
-            <img alt={'Icon for ' + tool.display_name} src={tool.image_url} />
-            <div className="tool-names">
-                <span className="tool-shortname">{tool.display_name}</span>
-                <span className="tool-longname">{tool.name}</span>
-            </div>
-        </a>
+        <Link href={tool.directUrl ? tool.directUrl : tool.url} >
+            <Card raised="true" className={classes.tool} p={1}>
+                <CardMedia component="img" image={tool.image_url} alt={'Icon for ' + tool.display_name} title={tool.display_name}/>
+                <CardContent p="1">
+                    <Typography p="3" variant="h5" component="h2">{tool.display_name}</Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">{tool.name}</Typography>
+                </CardContent>
+            </Card>
+        </Link>
     )
 }
