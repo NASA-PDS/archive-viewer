316afa5c9d6aadcadbd93eb87ab7c0357541d977
mdrum <mdrum@psi.edu>
2020-08-10 11:46:52 -0700

    rebuild sectioned tables/lists in material UI

--- src/components/BrowseTable.js
+++ src/components/BrowseTable.js
@@ -1,88 +1,95 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'css/BrowseTable.scss'
 import Loading from 'components/Loading.js'
+import { Box, Link,  List as MaterialList, ListItem, ListItemText, Typography, Paper, TableContainer, Table, TableBody, TableRow, TableCell } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
 /* ------ Constants ------ */
 const downplayGroupsThreshold = 100
 const hiddenGroupsThreshold = 1000
 const miscGroup = 'Other'
 
-class BrowseTables extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            expanded: false
+const useStyles = makeStyles((theme) => ({
+    sectionHeader: {
+      margin: theme.spacing(1)
+    },
+    table: {
+        border: 0
+    },
+    cell: {
+        borderLeft: 0,
+        borderRight: 0
+    }
+}));
+
+function BrowseTables(props) {
+    const [expanded, setExpanded] = useState(false)
+    const {items, sectioned, type} = props
+    const classes = useStyles();
+
+    if(!items) { return <Loading/>}
+    let groups = groupByAttributedRelationship(items)
+    groups.sort((a, b) => a.order < b.order ? -1 : 1)
+
+    let minorGroups = [], majorGroups = []
+    groups.forEach((group) => {
+        if(Number.isInteger(group.order)) {
+            if(group.order < downplayGroupsThreshold) { majorGroups.push(group) }
+            else if(group.order < hiddenGroupsThreshold) { minorGroups.push(group) }
         }
-    }
+    })
+    
+    if(majorGroups.length === 0 && minorGroups.length > 0) { setExpanded(true) }
 
-    toggleExpanded = event => {
-        event.preventDefault();
-        this.setState({ expanded: !this.state.expanded });
-    }
-
-    render() {
-        const {items} = this.props
-
-        if(!items) { return <Loading/>}
-        let groups = groupByAttributedRelationship(items)
-        groups.sort((a, b) => a.order < b.order ? -1 : 1)
-
-        let minorGroups = [], majorGroups = []
-        groups.forEach((group) => {
-            if(Number.isInteger(group.order)) {
-                if(group.order < downplayGroupsThreshold) { majorGroups.push(group) }
-                else if(group.order < hiddenGroupsThreshold) { minorGroups.push(group) }
+    return (
+        <Paper elevation={3} color="primary">
+            {majorGroups.map(group => { return (
+                <Box key={group.name}>
+                    <Typography variant="h4" className={classes.sectionHeader}>{group.name} {group.order < downplayGroupsThreshold ? type : ''}</Typography>
+                    { sectioned ? <SectionedTable items={group.items} /> : <List items={group.items} /> }
+                </Box>
+            )})}
+            {expanded === false && minorGroups.length > 0 && 
+                <Box className="browse-expand" onClick={() => setExpanded(true)}>See more</Box>
             }
-        })
-
-        const expanded = this.state.expanded || (majorGroups.length === 0 && minorGroups.length > 0)
-        return (
-            <div className="browse-tables">
-                {majorGroups.map(group => { return (
-                    <div key={group.name} className="major">
-                        <h2 className="browse-group-title">{group.name} {group.order < downplayGroupsThreshold ? this.type : ''}</h2>
-                        { this.sectioned ? <SectionedTable items={group.items} /> : <List items={group.items} /> }
-                    </div>
-                )})}
-                {expanded === false && minorGroups.length > 0 && 
-                    <div className="browse-expand" onClick={this.toggleExpanded}>See more</div>
-                }
-                {expanded && minorGroups.map(group => { return (
-                    <div key={group.name} className="minor">
-                        <h2 className="browse-group-title">{group.name} {group.order < downplayGroupsThreshold ? this.type : ''}</h2>
-                        { this.sectioned ? <SectionedTable items={group.items} /> : <List items={group.items} /> }
-                    </div>
-                )})}
-            </div>
-        )
-    }
+            {expanded && minorGroups.map(group => { return (
+                <Box key={group.name}>
+                    <Typography variant="h4">{group.name} {group.order < downplayGroupsThreshold ? type : ''}</Typography>
+                    { sectioned ? <SectionedTable items={group.items} /> : <List items={group.items} /> }
+                </Box>
+            )})}
+        </Paper>
+    )
 
 }
+export function SpacecraftBrowseTable(props) {
+    return <BrowseTables type="Spacecraft" sectioned={false} {...props}/>
+}
+export function InstrumentBrowseTable(props) {
+    return <BrowseTables type="Instruments" sectioned={true} {...props}/>
+}
 
-export class SpacecraftBrowseTable extends BrowseTables {
-    type = 'Spacecraft'
-    sectioned = false
-}
-export class InstrumentBrowseTable extends BrowseTables {
-    type = 'Instruments'
-    sectioned = true
-}
 function SectionedTable({items}) {
+    const classes = useStyles();
     let groups = groupByFirstTag(items)
     groups.sort((a, b) => a.order.localeCompare(b.order))
     return (
-        <div className="browse-table">
-            {groups.map(group => { return (
-                <div className="browse-row" key={group.name}>
-                    {(groups.length > 1 || group.name !== miscGroup) &&
-                        <div className="browse-col headers"> { group.name }</div>
-                    }
-                    <div className="browse-col">
-                        <List items={group.items} />
-                    </div>
-                </div>
-            )})}
-        </div>
+        <TableContainer>
+            <Table padding="none" className={classes.table}>
+                {groups.map(group => { return (
+                    <TableRow key={group.name}>
+                        <TableCell className={classes.cell} align="center" component="th" scope="row">
+                            {(groups.length > 1 || group.name !== miscGroup) &&
+                                <Typography variant="h6"> { group.name }</Typography>
+                            }
+                        </TableCell>
+                        <TableCell className={classes.cell}>
+                            <List items={group.items} />
+                        </TableCell>
+                    </TableRow>
+                )})}
+            </Table>
+        </TableContainer>
     )
 }
 
@@ -105,28 +112,28 @@ function List({items}) {
         return nameFinder(a).localeCompare(nameFinder(b))
     })
     return (
-        <ul className="list">
+        <MaterialList>
             {sortedItems.map((item,idx) => 
-                <li key={item.identifier + idx}><ItemLink item={item} single={false}/></li>
+                <ItemLink key={item.identifier + idx} item={item} single={false}/>
             )}
-        </ul>
+        </MaterialList>
     )
 }
 
 function ItemLink({item, single}) {
     let url = `?identifier=${item.identifier}`
     return (
-        <a href={url} className={single ? 'single-item' : ''}>
-            <span className="list-item-name">{ nameFinder(item) }</span>
-        </a>
+        <ListItem button component={Link} href={url}>
+            <ListItemText primary={ nameFinder(item) }/>
+        </ListItem>
     )
 }
 
 function NoItems() {
     return (
-        <div className="no-items">
+        <Box>
             <p>None of this type</p>
-        </div>
+        </Box>
     )
 }
 
