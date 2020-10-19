4b1979583eef2c46b1c6d2b2ece966e47c2c08b9
mdrum <mdrum@psi.edu>
2020-10-19 14:45:12 -0700

    clean up citation builder

--- src/components/CitationBuilder.js
+++ src/components/CitationBuilder.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { FormControl, InputLabel, Select, MenuItem, TextField, Grid } from '@material-ui/core'
+import { FormControl, InputLabel, Select, MenuItem, TextField, Grid, Typography, Box, Tooltip } from '@material-ui/core'
 import { Help } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -11,7 +11,7 @@ const FORMAT = {
 const buildCitation = (format, dataset) => {
     switch(format) {
         default:
-        case FORMAT.ADS:
+        case FORMAT.ADS: 
         case FORMAT.APA: return `${dataset.title} ${dataset.citation_publication_year} ${dataset.citation_author_list} ${dataset.citation_description}`
     }
 }
@@ -24,29 +24,41 @@ const useStyles = makeStyles((theme) => ({
     },
     citation: {
         width: '100%'
-    }
+    },
+    citationIcon: {
+        maxHeight: '50px',
+        maxWidth: '50px',
+    },
 }))
 
 export default function CitationBuilder({dataset}) {
     const [format, setFormat] = useState(FORMAT.ADS)
     const classes = useStyles()
 
-    return <Grid container direction="column" alignment="flex-start" spacing={1} className={classes.container}>
-        <Grid item>
-            <CitationSelector current={format} setter={setFormat}/>
-            <Help/>
+    return <Box m={1}>
+        <Typography variant="h3">Citation</Typography>
+        <Grid container direction="row" alignItems="flex-start">
+            <Grid item component="img" alt="" className={classes.citationIcon} src="./images/quotes-start.png" />
+            <Grid item style={{flexGrow: '1'}}>
+                <Grid container direction="column" spacing={1} className={classes.container}>
+                    <Grid item container spacing={1} direction="row" alignItems="center">
+                        <Grid item>
+                            <CitationSelector current={format} setter={setFormat}/>
+                        </Grid>
+                        <Grid item>
+                            <Tooltip placement="right" title="Select a format, then copy the citation out below">
+                                <Help/>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <TextField multiline rows={4} value={buildCitation(format, dataset)} variant="outlined" className={classes.citation}/>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item component="img" alt="" className={classes.citationIcon} style={{alignSelf: 'flex-end'}} src="./images/quotes-end.png" />
         </Grid>
-        <Grid item>
-            <TextField
-            multiline
-            rows={4}
-            value={buildCitation(format, dataset)}
-            variant="outlined"
-            className={classes.citation}
-            />
-        </Grid>
-    </Grid>
-    
+        </Box>
 }
 
 function CitationSelector({current, setter}) {
@@ -59,7 +71,7 @@ function CitationSelector({current, setter}) {
       label="Format"
     >
         {Object.values(FORMAT).map(format => (
-            <MenuItem value={format}>{format}</MenuItem>
+            <MenuItem key={format} value={format}>{format}</MenuItem>
         ))}
     </Select>
   </FormControl>
