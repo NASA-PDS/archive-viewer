9fcd9087f1dfd07e0a834b8b692b183dcd36f83e
mdrum <mdrum@psi.edu>
2021-01-20 17:20:21 -0700

    fix server side rendered missing css

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,15 @@ import AppBar from 'components/Banner'
 import 'fontsource-roboto'
 
 export default function MyApp({ Component, pageProps }) {
+
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+          jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
     <ThemeProvider theme={Theme}>
         <AppBar/>
