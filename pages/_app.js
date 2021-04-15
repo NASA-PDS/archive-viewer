4833a43af6a668a33e636d24e1deaff4939b1671
mdrum <mdrum@psi.edu>
2021-04-15 11:22:39 -0700

    remove traces of copy-pasted code

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 import 'css/FrontPage.scss'
-import 'css/main.scss';
-import { ThemeProvider } from '@material-ui/core/styles';
-import Theme from 'Theme';
+import 'css/main.scss'
+import { ThemeProvider } from '@material-ui/core/styles'
+import Theme from 'Theme'
 import AppBar from 'components/Banner'
 import 'fontsource-roboto'
-import { LinearProgress } from '@material-ui/core';
-import Router from "next/router";
+import { LinearProgress } from '@material-ui/core'
+import Router from "next/router"
 
 export default function MyApp({ Component, pageProps }) {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-          jssStyles.parentElement.removeChild(jssStyles);
+        // remove the server-side injected CSS
+        const jssStyles = document.querySelector('#jss-server-side')
+        if (!!jssStyles) {
+          jssStyles.parentElement.removeChild(jssStyles)
         }
-    }, []);
 
-    useEffect(() => {
+        // app-wide loading
         const start = () => {
-            setLoading(true);
-        };
+            setLoading(true)
+        }
         const end = () => {
-            setLoading(false);
-        };
-        Router.events.on("routeChangeStart", start);
-        Router.events.on("routeChangeComplete", end);
-        Router.events.on("routeChangeError", end);
-        return () => {
-            Router.events.off("routeChangeStart", start);
-            Router.events.off("routeChangeComplete", end);
-            Router.events.off("routeChangeError", end);
-        };
-    }, []);
+            setLoading(false)
+        }
+        Router.events.on("routeChangeStart", start)
+        Router.events.on("routeChangeComplete", end)
+        Router.events.on("routeChangeError", end)
+
+        return function cleanup() {
+            Router.events.off("routeChangeStart", start)
+            Router.events.off("routeChangeComplete", end)
+            Router.events.off("routeChangeError", end)
+        }
+    }, [])
 
 
     return (
