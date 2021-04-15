a9e90416948ee92a2185a2f740783629c507db26
mdrum <mdrum@psi.edu>
2021-04-15 11:20:55 -0700

    add app-wide loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'css/FrontPage.scss'
 import 'css/main.scss';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Theme from 'Theme';
 import AppBar from 'components/Banner'
 import 'fontsource-roboto'
+import { LinearProgress } from '@material-ui/core';
+import Router from "next/router";
 
 export default function MyApp({ Component, pageProps }) {
+    const [loading, setLoading] = useState(false);
 
-    React.useEffect(() => {
+    useEffect(() => {
         // Remove the server-side injected CSS.
         const jssStyles = document.querySelector('#jss-server-side');
         if (jssStyles) {
@@ -16,9 +19,28 @@ export default function MyApp({ Component, pageProps }) {
         }
     }, []);
 
+    useEffect(() => {
+        const start = () => {
+            setLoading(true);
+        };
+        const end = () => {
+            setLoading(false);
+        };
+        Router.events.on("routeChangeStart", start);
+        Router.events.on("routeChangeComplete", end);
+        Router.events.on("routeChangeError", end);
+        return () => {
+            Router.events.off("routeChangeStart", start);
+            Router.events.off("routeChangeComplete", end);
+            Router.events.off("routeChangeError", end);
+        };
+    }, []);
+
+
     return (
     <ThemeProvider theme={Theme}>
         <AppBar/>
+        { loading && <LinearProgress color="secondary" style={{position: 'absolute', width: '100%'}}/>}
         <Component {...pageProps} />
     </ThemeProvider>
     )
