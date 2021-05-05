66ec0d3d4f7e6296b8f9830849631025bdcb5d2b
mdrum <mdrum@psi.edu>
2021-05-05 16:23:01 -0700

    reorder mission html

--- components/pages/Mission.js
+++ components/pages/Mission.js
@@ -31,14 +31,12 @@ export default function Mission({mission, lidvid, pdsOnly}) {
                 <Typography variant="h1" gutterBottom> { mission.display_name ? mission.display_name : mission.title } </Typography>
                 <HTMLBox markup={mission.html1} />
                 <Metadata model={mission} tagType={TagTypes.mission}/>
-                <HTMLBox markup={mission.html2} />   
-                
-
                 { primaryBundle && 
                     <LabeledListItem label="Bundle" item={
                         <BundleLink identifier={primaryBundle.identifier} label="View Mission Information Bundle"/>
                     }/>
                 }
+                <HTMLBox markup={mission.html2} />   
 
             </>
         } />
