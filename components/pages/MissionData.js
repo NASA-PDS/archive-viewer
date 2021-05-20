2463a5f8b219ecf4769cbbe55cbdae6ed1b0c4b3
mdrum <mdrum@psi.edu>
2021-05-20 10:56:39 -0700

    add link to targets from the other data pages

--- components/pages/MissionData.js
+++ components/pages/MissionData.js
@@ -1,7 +1,10 @@
+import { Link, Typography } from '@material-ui/core';
 import Breadcrumbs from 'components/Breadcrumbs';
 import HTMLBox from 'components/HTMLBox';
+import InternalLink from 'components/InternalLink';
 import PrimaryLayout from 'components/PrimaryLayout';
 import React from 'react';
+import { pagePaths, types } from 'services/pages.js';
 
 
 
@@ -13,6 +16,8 @@ export default function MissionData({mission}) {
                 <Breadcrumbs currentTitle="Other Data" home={mission}/>
 
                 <HTMLBox markup={mission.other_html}/>
+
+                <Typography align="center" color="textSecondary">Additional derived data may be available on the this mission's <InternalLink identifier={mission.identifier} additionalPath={pagePaths[types.MISSIONTARGETS]} passHref><Link>target information pages</Link></InternalLink></Typography>
             </>
         } />
     )
