d7041fe21b918d6f9c13732096f82ffe60df2517
mdrum <mdrum@psi.edu>
2021-06-01 14:34:14 -0700

    change favicons

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,10 @@ class MyDocument extends Document {
                 <script src="/feedback/js/config.js"></script>
                 <script src="/feedback/js/feedback.js"></script>
                 
+                <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png"/>
+                
                 <script src="https://pds.nasa.gov/pds-app-bar/pds-app-bar.js"></script>
                 <link rel="stylesheet" href="https://pds.nasa.gov/pds-app-bar/pds-app-bar.css"  type="text/css" media="screen" />
                 
