7ffce826f39e4f3a2e82b85d3a4858eb8643ae50
mdrum <mdrum@psi.edu>
2021-06-04 11:33:20 -0700

    make sure collection type always gets treated as array

--- services/desolrize.js
+++ services/desolrize.js
@@ -40,7 +40,7 @@ function unpackChildDocuments(document, children) {
     }
 }
 
-const keysThatAreActuallyStringArrays = ['tags', 'target_ref', 'instrument_ref', 'instrument_host_ref', 'investigation_ref', 'collection_ref', 'download_packages', 'superseded_data', 'related_tools', 'related_data']
+const keysThatAreActuallyStringArrays = ['tags', 'target_ref', 'instrument_ref', 'instrument_host_ref', 'investigation_ref', 'collection_ref', 'download_packages', 'superseded_data', 'related_tools', 'related_data', 'collection_type']
 const keysThatAreActuallyObjectArrays = ['related_tools', 'related_data', 'superseded_data', 'download_packages']
 const keysThatAreAlwaysSingularChildDocuments = ['example', 'publication']
 const keysThatAreNeverArrays = ['is_major', 'is_prime', 'order', 'toolId']
