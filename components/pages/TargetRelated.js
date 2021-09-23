161b673924f5e9e2925b25100289cdc71a483b1d
mdrum <mdrum@psi.edu>
2021-09-23 11:54:53 -0700

    include form controls for multiple/inclusive filter flags

--- components/pages/TargetRelated.js
+++ components/pages/TargetRelated.js
@@ -1,4 +1,4 @@
-import { Typography, Box, Paper, Grid, FormControl, InputLabel, Select, MenuItem, ListItemText, Chip, Checkbox } from '@material-ui/core';
+import { Typography, Box, Paper, Grid, FormControl, InputLabel, Select, MenuItem, ListItemText, Chip, Checkbox, FormControlLabel, Switch, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { getRelatedTargetsForTarget } from 'api/target';
 import Breadcrumbs from 'components/Breadcrumbs';
@@ -11,8 +11,7 @@ import { groupByField } from 'services/groupings';
 const useStyles = makeStyles((theme) => ({
     formControl: {
         margin: theme.spacing(1),
-        minWidth: 250,
-        maxWidth: 400,
+        minWidth: 250
     },
     chips: {
         display: 'flex',
@@ -29,11 +28,12 @@ const useStyles = makeStyles((theme) => ({
 export default function TargetRelated(props) {
     const { target } = props
     const [relatedTargets, setRelatedTargets] = useState(null)
-    
-    const tags = relatedTargets?.flatMap(target => target.tags || []).sort((a, b) => a.name.localeCompare(b.name))
-    const groups = groupByField(tags || [], 'group').sort((a, b) => a.order < b.order ? -1 : 1)
-    const [filterTags, setFilterTags] = React.useState(groups.map(g => []));
-
+    const [multipleGroupMode, setMultipleGroupMode] = useState(false)
+    const [inclusiveOrMode, setInclusiveOrMode] = useState(false)
+    const [filterState, setFilterState] = React.useState({
+        tags: null,
+        activeGroup: null
+    })
     useEffect(() => {
         getRelatedTargetsForTarget(target).then(setRelatedTargets, er => console.error(er))
         return function cleanup() {
@@ -41,15 +41,44 @@ export default function TargetRelated(props) {
         }
     }, [props.target])
 
-    const updateFilter = (index) => {
-        return (tags) => {
-            filterTags.splice(index, 1, tags)
-            setFilterTags([...filterTags])
+    let filteredTargets = relatedTargets, groups = []
+    if(relatedTargets?.length > 0) {
+
+        const tags = relatedTargets?.flatMap(target => target.tags || []).sort((a, b) => a.name.localeCompare(b.name))
+        groups = groupByField(tags || [], 'group').sort((a, b) => a.order < b.order ? -1 : 1)
+        if(!filterState.tags && groups.length > 0) {
+            setFilterState(Object.assign(filterState, {
+                tags: Array(groups.length).fill([])
+            }))
+        } 
+    
+        if(filterState.tags.flat().length === 0) {
+            filteredTargets = relatedTargets
+        }
+        else if(!multipleGroupMode) {
+            const filterTags = !filterState.activeGroup ? filterState.tags.flat() : filterState.tags[filterState.activeGroup]
+            filteredTargets = relatedTargets?.filter(target => target.tags?.some(tag => filterTags.includes(tag.name)))
+        }
+        else if(multipleGroupMode && inclusiveOrMode) {
+            const filterTags = filterState.tags.flat()
+            filteredTargets = relatedTargets?.filter(target => target.tags?.some(tag => filterTags.includes(tag.name)))
+        } else {
+            filteredTargets = relatedTargets?.filter(target => filterState.tags.every(tagGroup => {
+                return target.tags?.some(tag => tagGroup.length === 0 || tagGroup.includes(tag.name))
+            }))
         }
     }
 
-    const filterTagsFlat = filterTags.flat()
-    const filteredTargets = filterTagsFlat.length > 0 ? relatedTargets?.filter(target => target.tags?.some(tag => filterTagsFlat.includes(tag.name))) : relatedTargets
+    const updateFilter = (index) => {
+        return (tags) => {
+            filterState.tags.splice(index, 1, tags)
+            setFilterState({
+                tags: filterState.tags,
+                activeGroup: index
+            })
+        }
+    }
+    
 
     return (
         <PrimaryLayout primary={
@@ -58,10 +87,30 @@ export default function TargetRelated(props) {
                 <Typography variant="h1" gutterBottom>Related Targets</Typography>
 
                 <Paper component={Box} px={2} py={1}>
-                <Grid container direction="row" my={2} alignItems="center">
-                    <Typography variant="h4">Filter:</Typography>
-                    {groups.map((group, index) => <TagSelector key={group.name} label={group.name} tags={group.items} filter={updateFilter(index)}/>)}
-                </Grid>
+                    <Grid container direction="row" my={2} alignItems="center" wrap="nowrap">
+                        <Typography variant="h4">Filter:</Typography>
+                        {groups.map((group, index) => <TagSelector key={group.name} label={group.name} tags={group.items} filter={updateFilter(index)} disabled={!multipleGroupMode && filterState.activeGroup !== index}/>)}
+                        <Box marginLeft="auto">
+                            <Tooltip placement="top" title="Off: filter by the most recently used. On: use all filters simultaneously.">
+                            <FormControlLabel
+                                value="bottom"
+                                control={<Switch checked={multipleGroupMode} onChange={() => setMultipleGroupMode(!multipleGroupMode)} size="small" color="primary" />}
+                                label="Use multiple"
+                                labelPlacement="bottom"
+                            />
+                            </Tooltip>
+                        </Box>
+                        <Box>
+                            <Tooltip placement="top" title="Off: results must have at least one tag of each group. On: results can have any of the tags.">
+                            <FormControlLabel
+                                value="bottom"
+                                control={<Switch checked={inclusiveOrMode} onChange={() => setInclusiveOrMode(!inclusiveOrMode)} size="small" color="primary" disabled={!multipleGroupMode}/>}
+                                label="Inclusive"
+                                labelPlacement="bottom"
+                            />
+                            </Tooltip>
+                        </Box>
+                    </Grid>
                 </Paper>
 
                 <LoadingWrapper model={relatedTargets}>
@@ -86,7 +135,8 @@ const MenuProps = {
         },
     },
 };
-function TagSelector({label, tags, filter}) {
+
+function TagSelector({label, tags, filter, disabled}) {
     const classes = useStyles()
     const [selected, setSelected] = React.useState([]);
 
@@ -111,7 +161,7 @@ function TagSelector({label, tags, filter}) {
                     renderValue={(selected) => (
                         <Box className={classes.chips} >
                           {selected.map((value, index) => (
-                            index < 2 && <Chip color="secondary" key={value} label={value} className={classes.chip}/>
+                            index < 2 && <Chip color={disabled ? 'default' : 'secondary'} key={value} label={value} className={classes.chip}/>
                           ))}
                           {selected.length > 2 && <Chip label={`${selected.length - 2} more...`} className={classes.chip}/>}
                         </Box>
