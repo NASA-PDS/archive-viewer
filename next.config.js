372e1cac4fd385ac228776809b314e8fb6b0b498
mdrum <mdrum@psi.edu>
2022-03-10 10:35:15 -0700

    redo migration to next 12 by adding missing modules webpack removed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 module.exports = {
     webpack: (config) => {
-        config.resolve.fallback = { fs:  false, path: false}
+        config.resolve.fallback = { fs:  false, path: false, buffer: false, process: require.resolve("process/browser"), events: require.resolve("events/") }
         return config
  
       },
+      
 }
