c2d194871522a4e5b8e6dbb49e0586654ef4912d
mdrum <mdrum@psi.edu>
2022-03-11 15:02:04 -0700

    specify docker node version and platform architecture

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,5 @@
 # Build the docker image
-docker build -t sbnpsi/context-browser:latest .
+docker buildx build --platform linux/amd64 -t sbnpsi/context-browser:latest .
 
 # Push to dockerhub
 docker push sbnpsi/context-browser:latest
